a3398828edce0b3ff8cf98ddf008689cc7f342c7
dveleztx <dveleztx@gmail.com>
2018-06-21 13:04:17 -0700

    got print_header and event_loop written out

---
+++ program.py
@@ -0,0 +1,31 @@
+
+def main():
+    print_header()
+    run_event_loop()
+
+
+def print_header():
+    print('---------------------------')
+    print('       JOURNAL APP')
+    print('---------------------------')
+
+
+def run_event_loop():
+
+    print('What do you want to do with your journal?')
+    cmd = None
+
+    while cmd != 'x':
+        cmd = input('[L]ist entries, [A]dd an entry, E[x]it: ')
+        cmd = cmd.lower().strip()
+
+        if cmd == 'l':
+            print('L')
+        elif cmd == 'a':
+            print('A')
+        elif cmd != 'x':
+            print("Sorry, we don't understand")
+
+    print('Done, goodbye')
+
+main()
