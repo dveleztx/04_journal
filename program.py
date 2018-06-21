2107e91b6c052e84bbdf965ed60d4a9cca76ee23
dveleztx <dveleztx@gmail.com>
2018-06-21 13:41:02 -0700

    created functions for list and adding journal entries while cleaning up their output

--- program.py
+++ program.py
@@ -1,4 +1,3 @@
-
 def main():
     print_header()
     run_event_loop()
@@ -11,21 +10,34 @@ def print_header():
 
 
 def run_event_loop():
-
     print('What do you want to do with your journal?')
     cmd = None
+    journal_data = []  # list()
 
     while cmd != 'x':
         cmd = input('[L]ist entries, [A]dd an entry, E[x]it: ')
         cmd = cmd.lower().strip()
 
         if cmd == 'l':
-            print('L')
+            list_entries(journal_data)
         elif cmd == 'a':
-            print('A')
+            add_entry(journal_data)
         elif cmd != 'x':
             print("Sorry, we don't understand")
 
     print('Done, goodbye')
+
+
+def list_entries(data):
+    print('Your journal entries')
+    entries = reversed(data)
+    for idx, entry in enumerate(entries):
+        print('* [{}] {}'.format(idx + 1, entry))
+
+
+def add_entry(data):
+    text = input('Type your entry, <enter> to exit: ')
+    data.append(text)
+
 
 main()
